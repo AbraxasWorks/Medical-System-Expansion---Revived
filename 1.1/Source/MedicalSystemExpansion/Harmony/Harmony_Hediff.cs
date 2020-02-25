1e3cbecd7268e8df7a01dfbcb878ea8c5e3a5e34
Abraxas <abraxasbraxworks@gmail.com>
2020-02-25 12:44:46 +0800

    1.1

---
+++ 1.1/Source/MedicalSystemExpansion/Harmony/Harmony_Hediff.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace OrenoMSE.Harmony2
+{
+    public class Harmony_Hediff
+    {
+        [HarmonyPatch(typeof(Hediff))]
+        [HarmonyPatch("LabelBase", MethodType.Getter)]
+        internal class Hediff_LabelBase
+        {
+            [HarmonyPostfix]
+            [HarmonyPriority(Priority.Last)]
+            private static void HediffPrettyLabel(ref string __result, Hediff __instance)
+            {
+                if (__instance is Hediff_AddedPart || __instance is Hediff_Implant)
+                {
+                    __result = MSE_VanillaExtender.PrettyLabel(__instance);
+                }
+            }
+        }
+    }
+}
---
+++ 1.1/Source/MedicalSystemExpansion/MSE_DefModExtension/MSE_HediffPrettyLabel.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace OrenoMSE
+{
+    public class MSE_HediffPrettyLabel : DefModExtension
+    {
+        public string labelPretty;
+
+        public string genderlessNoun;
+
+        public string maleNoun;
+
+        public string femaleNoun;
+    }
+}
