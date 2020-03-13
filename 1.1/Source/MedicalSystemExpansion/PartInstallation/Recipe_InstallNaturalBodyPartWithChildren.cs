2c811469c4da794103e3542ab7cddddae5b03294
JonM0 <gio.moschini2000@gmail.com>
2020-03-13 17:25:05 +0100

    prevent installation of natural parts on bionics

--- 1.1/Source/MedicalSystemExpansion/PartInstallation/Recipe_InstallNaturalBodyPartWithChildren.cs
+++ 1.1/Source/MedicalSystemExpansion/PartInstallation/Recipe_InstallNaturalBodyPartWithChildren.cs
@@ -10,6 +10,14 @@ namespace OrenoMSE.PartInstallation
 	class Recipe_InstallNaturalBodyPartWithChildren : Recipe_InstallNaturalBodyPart
 	{
 
+		public override IEnumerable<BodyPartRecord> GetPartsToApplyOn ( Pawn pawn, RecipeDef recipe )
+		{
+			// don't add natural parts to prosthetics
+			return from part in base.GetPartsToApplyOn( pawn, recipe )
+				   where part.parent == null || pawn.health.hediffSet.HasDirectlyAddedPartFor(part.parent)
+				   select part;
+		}
+
 		public override void ApplyOnPawn ( Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill )
 		{
 			// START VANILLA CODE (couldn't know if the surgery was successfull)
