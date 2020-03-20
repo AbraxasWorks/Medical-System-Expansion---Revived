8bc53a6fb0fb6c8aceeee3a8bbd74789730c2af9
JonM0 <gio.moschini2000@gmail.com>
2020-03-20 15:11:41 +0100

    Disable harmony patches to hook the applyextrahediffs modextension

--- 1.1/Source/MedicalSystemExpansion/Modules/Actual/Recipe_InstallModule.cs
+++ 1.1/Source/MedicalSystemExpansion/Modules/Actual/Recipe_InstallModule.cs
@@ -11,25 +11,25 @@ namespace OrenoMSE.Modules.Actual
         public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
         {
 			return
-			// in the parts that the recipe can be applied to and that the pawn has
-			from BodyPartRecord bodyPart in
-				from rbpd in recipe.appliedOnFixedBodyParts
-				from abpr in pawn.health.hediffSet.GetNotMissingParts()
-				where rbpd == abpr.def
-				select abpr
-			where 
-				pawn.health.hediffSet.GetHediffs<Hediff_ModuleAbstract>().Any(
-						delegate ( Hediff_ModuleAbstract hediff_Module )
-						{
-							return hediff_Module.Part == bodyPart;   // in that part there is any module or slot
-						} )
-			where
-				! pawn.health.hediffSet.GetHediffs<Hediff_ModuleAbstract>().Any(
-						delegate ( Hediff_ModuleAbstract hediff_Module )
-						{
-							return hediff_Module.def == recipe.addsHediff;   // part doesn't have the module i'm trying to add
-						} )
-			select bodyPart;
+				// in the parts that the recipe can be applied to and that the pawn has
+				from BodyPartRecord bodyPart in
+					from rbpd in recipe.appliedOnFixedBodyParts
+					from abpr in pawn.health.hediffSet.GetNotMissingParts()
+					where rbpd == abpr.def
+					select abpr
+				where 
+					pawn.health.hediffSet.GetHediffs<Hediff_ModuleAbstract>().Any(
+							delegate ( Hediff_ModuleAbstract hediff_Module )
+							{
+								return hediff_Module.Part == bodyPart;   // in that part there is any module or slot
+							} )
+				where
+					! pawn.health.hediffSet.GetHediffs<Hediff_ModuleAbstract>().Any(
+							delegate ( Hediff_ModuleAbstract hediff_Module )
+							{
+								return hediff_Module.def == recipe.addsHediff;   // part doesn't have the module i'm trying to add
+							} )
+				select bodyPart;
 		}
 
 		public override void ApplyOnPawn ( Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill )
