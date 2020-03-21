6aa7812db70982e5af9c8b406d9469b9273969e5
JonM0 <gio.moschini2000@gmail.com>
2020-03-21 18:09:44 +0100

    Created new xml patch operation to add to a list

---
+++ 1.1/Source/MedicalSystemExpansion/XpathPatches/PatchOperationAddToList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using Verse;
+
+namespace OrenoMSE.XpathPatches
+{
+    class PatchOperationAddToList : PatchOperationPathed
+    {
+        protected string list;
+
+        private XmlContainer value;
+
+        protected override bool ApplyWorker ( XmlDocument xml )
+        {
+            XmlNode valNode = value.node;
+            bool result = false;
+
+
+            foreach ( XmlNode parentNode in from object x in xml.SelectNodes( xpath )
+                                            select (XmlNode)x ) 
+            {
+                result = true;
+
+                XmlNode listNode = parentNode.SelectSingleNode( list );
+                if ( listNode == null )
+                {
+                    // Add - Add list if not existing
+                    listNode = parentNode.OwnerDocument.CreateElement( list );
+                    parentNode.AppendChild( listNode );
+                }
+
+                // finally add element to list
+                listNode.AppendChild( parentNode.OwnerDocument.ImportNode(valNode.FirstChild, true) );
+            }
+
+
+            return result;
+        }
+
+    }
+}
