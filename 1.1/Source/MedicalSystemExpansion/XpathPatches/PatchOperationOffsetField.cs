a76badf726628839b7b6fb7d8425001bac252e86
JonM0 <gio.moschini2000@gmail.com>
2020-03-21 19:04:47 +0100

    Improved royalty research

---
+++ 1.1/Source/MedicalSystemExpansion/XpathPatches/PatchOperationOffsetField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using Verse;
+
+namespace OrenoMSE.XpathPatches
+{
+    class PatchOperationOffsetField : PatchOperationPathed
+    {
+
+        private double offset;
+
+        protected override bool ApplyWorker ( XmlDocument xml )
+        {
+
+            bool result = false;
+
+
+            foreach ( XmlNode parentNode in from object x in xml.SelectNodes( xpath )
+                                            select (XmlNode)x )
+            {
+                result = true;
+
+                double oldValue = double.Parse( parentNode.InnerText );
+
+                parentNode.InnerText = string.Format( "{0}", oldValue + offset );
+            }
+
+
+            return result;
+        }
+
+    }
+}
