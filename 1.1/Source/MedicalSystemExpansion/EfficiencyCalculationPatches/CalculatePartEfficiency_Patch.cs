abd1cdb93f88303eb7e2945f40970b098441b2c7
JonM0 <gio.moschini2000@gmail.com>
2020-03-26 15:09:57 +0100

    fixed part efficiency calculation

--- 1.1/Source/MedicalSystemExpansion/EfficiencyCalculationPatches/CalculatePartEfficiency_Patch.cs
+++ 1.1/Source/MedicalSystemExpansion/EfficiencyCalculationPatches/CalculatePartEfficiency_Patch.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -42,5 +43,21 @@ namespace OrenoMSE.EfficiencyCalculationPatches
 			}
 			return true;
 		}
+
+
+
+		// remove the copying of the efficiency of a parent added part
+
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler ( IEnumerable<CodeInstruction> instructions )
+		{
+			Label l = (Label) instructions.First( ( CodeInstruction i ) => i.opcode == OpCodes.Br_S ).operand; // target of the first branch in the first for loop
+
+			int firstBranchJump = instructions.FirstIndexOf( ( CodeInstruction i ) => i.labels.Contains(l) ); // the location of that instruction
+			firstBranchJump += 3; // the for ends 3 instructions later
+
+			return instructions.Skip( firstBranchJump ); // skip all instructions of the first for
+		}
+
 	}
 }
