4fa4141a98e2bb264daf5a0e55b48743f98b1135
JonM0 <gio.moschini2000@gmail.com>
2020-03-27 17:22:36 +0100

    implemented transpiler to CacheMissingPartsCommonAncestors_Patch

--- 1.1/Source/MedicalSystemExpansion/EfficiencyCalculationPatches/CacheMissingPartsCommonAncestors_Patch.cs
+++ 1.1/Source/MedicalSystemExpansion/EfficiencyCalculationPatches/CacheMissingPartsCommonAncestors_Patch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,50 +22,68 @@ namespace OrenoMSE.EfficiencyCalculationPatches
 		}
 
 
-		// Patch to show that child parts of prosthetics are missing in the health tab
-		// should probably implement a transpiler
+		// Patch to show that child parts of prosthetics are missing in the health tab and to hide ignored parts
 
-		[HarmonyPrefix]
-		public static bool PreFix ( HediffSet __instance, ref List<Hediff_MissingPart> ___cachedMissingPartsCommonAncestors, ref Queue<BodyPartRecord> ___missingPartsCommonAncestorsQueue )
+		[HarmonyTranspiler]
+		public static IEnumerable<CodeInstruction> Transpiler ( IEnumerable<CodeInstruction> instructions )
 		{
-			
-			if ( ___cachedMissingPartsCommonAncestors == null )
+			foreach ( CodeInstruction instruction in instructions )
 			{
-				___cachedMissingPartsCommonAncestors = new List<Hediff_MissingPart>();
-			}
-			else
-			{
-				___cachedMissingPartsCommonAncestors.Clear();
-			}
-			___missingPartsCommonAncestorsQueue.Clear();
-			___missingPartsCommonAncestorsQueue.Enqueue( __instance.pawn.def.race.body.corePart );
-			while ( ___missingPartsCommonAncestorsQueue.Count != 0 )
-			{
-				BodyPartRecord node = ___missingPartsCommonAncestorsQueue.Dequeue();
-
-				// show even if child of added part
-				if ( !__instance.ParentPartIgnores( node ) ) //!__instance.PartOrAnyAncestorHasDirectlyAddedParts( node ) )
+				// substitute call to PartOrAnyAncestorHasDirectlyAddedParts with a call to ParentPartIgnores
+				if ( instruction.Calls( typeof( HediffSet ).GetMethod( nameof( HediffSet.PartOrAnyAncestorHasDirectlyAddedParts ) ) ) )
 				{
-					Hediff_MissingPart hediff_MissingPart = (from x in __instance.GetHediffs<Hediff_MissingPart>()
-																where x.Part == node
-																select x).FirstOrDefault<Hediff_MissingPart>();
-					if ( hediff_MissingPart != null )
-					{
-						___cachedMissingPartsCommonAncestors.Add( hediff_MissingPart );
-					}
-					else
-					{
-						for ( int i = 0; i < node.parts.Count; i++ )
-						{
-							___missingPartsCommonAncestorsQueue.Enqueue( node.parts[i] );
-						}
-					}
+					yield return new CodeInstruction( OpCodes.Callvirt, typeof( Utilities ).GetMethod( nameof( Utilities.ParentPartIgnores ) ) );
 				}
+
+				else yield return instruction;
 			}
-
-
-
-			return false;
+			yield break;
 		}
+
+
+		// equivalent change:
+
+		//[HarmonyPrefix]
+		//public static bool PreFix ( HediffSet __instance, ref List<Hediff_MissingPart> ___cachedMissingPartsCommonAncestors, ref Queue<BodyPartRecord> ___missingPartsCommonAncestorsQueue )
+		//{
+			
+		//	if ( ___cachedMissingPartsCommonAncestors == null )
+		//	{
+		//		___cachedMissingPartsCommonAncestors = new List<Hediff_MissingPart>();
+		//	}
+		//	else
+		//	{
+		//		___cachedMissingPartsCommonAncestors.Clear();
+		//	}
+		//	___missingPartsCommonAncestorsQueue.Clear();
+		//	___missingPartsCommonAncestorsQueue.Enqueue( __instance.pawn.def.race.body.corePart );
+		//	while ( ___missingPartsCommonAncestorsQueue.Count != 0 )
+		//	{
+		//		BodyPartRecord node = ___missingPartsCommonAncestorsQueue.Dequeue();
+
+		//		// show even if child of added part
+		//		if ( !__instance.ParentPartIgnores( node ) ) //!__instance.PartOrAnyAncestorHasDirectlyAddedParts( node ) )
+		//		{
+		//			Hediff_MissingPart hediff_MissingPart = (from x in __instance.GetHediffs<Hediff_MissingPart>()
+		//														where x.Part == node
+		//														select x).FirstOrDefault<Hediff_MissingPart>();
+		//			if ( hediff_MissingPart != null )
+		//			{
+		//				___cachedMissingPartsCommonAncestors.Add( hediff_MissingPart );
+		//			}
+		//			else
+		//			{
+		//				for ( int i = 0; i < node.parts.Count; i++ )
+		//				{
+		//					___missingPartsCommonAncestorsQueue.Enqueue( node.parts[i] );
+		//				}
+		//			}
+		//		}
+		//	}
+
+
+
+		//	return false;
+		//}
 	}
 }
