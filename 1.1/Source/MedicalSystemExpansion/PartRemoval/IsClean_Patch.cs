f13a9c04e5f29026ccc7ea2c75f784ad9651fb0e
JonM0 <gio.moschini2000@gmail.com>
2020-03-28 15:14:02 +0100

    implants keep parts clean

---
+++ 1.1/Source/MedicalSystemExpansion/PartRemoval/IsClean_Patch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+
+namespace OrenoMSE.PartRemoval
+{
+
+    // makes it so parts with implants are considered clean
+
+    [HarmonyPatch(typeof(MedicalRecipesUtility))]
+    [HarmonyPatch( "IsClean" )]
+    public static class IsClean_Patch
+    {
+        [HarmonyPostfix]
+        public static void PostFix ( ref bool __result, Pawn pawn, BodyPartRecord part )
+        {
+            // pawn is alive and the part doesn't have any diff that is not an implant
+            __result =
+                __result ||
+                !pawn.Dead && !(from x in pawn.health.hediffSet.hediffs
+                                where x.Part == part
+                                where !(x is Hediff_Implant)
+                                select x).Any();
+        }
+    }
+}
