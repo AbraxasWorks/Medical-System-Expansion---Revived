ac1084a175ad230beacfda844b1633ea4af8c6ac
JonM0 <gio.moschini2000@gmail.com>
2020-03-29 01:12:44 +0100

    disabled healing and bleeding on injured prosthetics

--- 1.1/Source/MedicalSystemExpansion/DontHealInjury/Harmony_HediffUtility.cs
+++ 1.1/Source/MedicalSystemExpansion/DontHealInjury/Harmony_HediffUtility.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 using Verse;
 
@@ -13,18 +15,15 @@ namespace OrenoMSE.HarmonyPatches
             [HarmonyPostfix]
             public static void DontHealInjury(ref bool __result, Hediff hd)
             {
-                foreach ( Hediff hediff in hd.pawn.health.hediffSet.hediffs )
-                {
-                    if ( hediff.Part == hd.Part && hediff.def.HasModExtension<MSE_DontHealInjury>())
-                    {
-                        MSE_DontHealInjury dontHealInjury = hediff.def.GetModExtension<MSE_DontHealInjury>();
-                        if (dontHealInjury != null && dontHealInjury.notHealInjury)
-                        {
-                            __result = true;
-                            return;
-                        }
-                    }
-                }
+
+                // makes it so injuries on hediff_addedpart are permanent (they also cannot bleed)
+
+                __result = __result ||
+                    (from h in hd.pawn.health.hediffSet.hediffs
+                     where h.Part == hd.Part
+                     where h is Hediff_AddedPart
+                     select h).Any();
+                
             }
         }
     }
