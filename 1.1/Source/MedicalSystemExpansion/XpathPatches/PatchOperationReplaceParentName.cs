ec7c3d8900264dd09a249deb23f0ab89c492bd3f
JonM0 <gio.moschini2000@gmail.com>
2020-04-04 14:45:54 +0200

    commented out useless shit

--- 1.1/Source/MedicalSystemExpansion/XpathPatches/PatchOperationReplaceParentName.cs
+++ 1.1/Source/MedicalSystemExpansion/XpathPatches/PatchOperationReplaceParentName.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using Verse;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml;
+//using Verse;
 
-namespace OrenoMSE.XpathPatches
-{
-    class PatchOperationReplaceParentName : PatchOperationPathed
-    {
-        protected string parentxpath;
+//namespace OrenoMSE.XpathPatches
+//{
+//    class PatchOperationReplaceParentName : PatchOperationPathed
+//    {
+//        protected string parentxpath;
 
-        protected override bool ApplyWorker ( XmlDocument xml )
-        {
+//        protected override bool ApplyWorker ( XmlDocument xml )
+//        {
 
-            bool result = false;
+//            bool result = false;
             
-            var possibleReplacements = xml.SelectNodes( parentxpath );
+//            var possibleReplacements = xml.SelectNodes( parentxpath );
 
-            if ( possibleReplacements.Count != 1 ) return false;
+//            if ( possibleReplacements.Count != 1 ) return false;
 
-            XmlNode replacementNode = possibleReplacements.Item( 0 );
-
-
-            foreach ( XmlNode node in from object x in xml.SelectNodes( xpath )
-                                            select (XmlNode)x )
-            {
-
-                if ( node.Attributes["ParentName"] != null )
-                {
-                    node.ParentNode.SelectSingleNode();
+//            XmlNode replacementNode = possibleReplacements.Item( 0 );
 
 
-                }
+//            foreach ( XmlNode node in from object x in xml.SelectNodes( xpath )
+//                                            select (XmlNode)x )
+//            {
+
+//                if ( node.Attributes["ParentName"] != null )
+//                {
+//                    node.ParentNode.SelectSingleNode();
 
 
-                result = true;
-            }
+//                }
 
 
-            return result;
-        }
-    }
-}
+//                result = true;
+//            }
+
+
+//            return result;
+//        }
+//    }
+//}
