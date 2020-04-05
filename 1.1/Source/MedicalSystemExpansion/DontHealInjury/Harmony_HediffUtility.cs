05e09bfd3c5ac3d2b9aabe003dbe80f1cecbaa16
JonM0 <gio.moschini2000@gmail.com>
2020-04-05 18:30:45 +0200

    commented out donthealinjury

--- 1.1/Source/MedicalSystemExpansion/DontHealInjury/Harmony_HediffUtility.cs
+++ 1.1/Source/MedicalSystemExpansion/DontHealInjury/Harmony_HediffUtility.cs
@@ -1,30 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using HarmonyLib;
-using Verse;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using HarmonyLib;
+//using Verse;
 
-namespace OrenoMSE.HarmonyPatches
-{
-    public class Harmony_HediffUtility
-    {
-        [HarmonyPatch(typeof(HediffUtility))]
-        [HarmonyPatch("IsPermanent")]
-        internal class HediffUtility_IsPermanent
-        {
-            [HarmonyPostfix]
-            public static void DontHealInjury(ref bool __result, Hediff hd)
-            {
+//namespace OrenoMSE.HarmonyPatches
+//{
+//    public class Harmony_HediffUtility
+//    {
+//        [HarmonyPatch(typeof(HediffUtility))]
+//        [HarmonyPatch("IsPermanent")]
+//        internal class HediffUtility_IsPermanent
+//        {
+//            [HarmonyPostfix]
+//            public static void DontHealInjury(ref bool __result, Hediff hd)
+//            {
 
-                // makes it so injuries on hediff_addedpart are permanent (they also cannot bleed)
+//                // makes it so injuries on hediff_addedpart are permanent (they also cannot bleed)
 
-                __result = __result ||
-                    (from h in hd.pawn.health.hediffSet.hediffs
-                     where h.Part == hd.Part
-                     where h is Hediff_AddedPart
-                     select h).Any();
+//                __result = __result ||
+//                    (from h in hd.pawn.health.hediffSet.hediffs
+//                     where h.Part == hd.Part
+//                     where h is Hediff_AddedPart
+//                     select h).Any();
                 
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//}
