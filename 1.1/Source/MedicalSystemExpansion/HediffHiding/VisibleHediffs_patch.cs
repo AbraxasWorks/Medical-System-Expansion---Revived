b2ac9c099bba36f19d9d54288f226a23bd2dc374
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-04-12 12:51:30 +0200

    added patch to hide hediffs

---
+++ 1.1/Source/MedicalSystemExpansion/HediffHiding/VisibleHediffs_patch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using System.Reflection;
+
+namespace OrenoMSE.HediffHiding
+{
+    [HarmonyPatch]
+    internal static class VisibleHediffs_patch
+    {
+        private static MethodBase TargetMethod ()
+        {
+            return AccessTools.Method( typeof( HealthCardUtility ), "VisibleHediffs" );
+        }
+
+        [HarmonyPostfix]
+        private static void PostFix ( ref IEnumerable<Hediff> __result, Pawn pawn, bool ___showAllHediffs )
+        {
+            if ( !___showAllHediffs )
+            {
+                __result = from h in __result
+                           where !(h is Hediff_AddedPart)
+                            || !pawn.health.hediffSet.HasDirectlyAddedPartFor( h.Part.parent )
+                            || pawn.health.hediffSet.GetInjuredParts().Contains( h.Part )
+                           select h;
+            }
+        }
+    }
+}
