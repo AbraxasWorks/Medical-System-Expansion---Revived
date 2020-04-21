7b98da424b6d01c1f7901445e506d27ce807614a
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-04-21 19:43:07 +0200

    fixed natural part partstoapplyon

--- 1.1/Source/MedicalSystemExpansion/PartInstallation/Recipe_InstallNaturalBodyPartWithChildren.cs
+++ 1.1/Source/MedicalSystemExpansion/PartInstallation/Recipe_InstallNaturalBodyPartWithChildren.cs
@@ -12,9 +12,20 @@ namespace MSE2
         public override IEnumerable<BodyPartRecord> GetPartsToApplyOn ( Pawn pawn, RecipeDef recipe )
         {
             // don't add natural parts to prosthetics
-            return from part in base.GetPartsToApplyOn( pawn, recipe )
-                   where part.parent == null || pawn.health.hediffSet.HasDirectlyAddedPartFor( part.parent )
-                   select part;
+
+            return
+                MedicalRecipesUtility.GetFixedPartsToApplyOn( recipe, pawn,
+                    ( BodyPartRecord record ) =>
+                    // part has some hediff
+                    pawn.health.hediffSet.hediffs.Any( ( Hediff x ) => x.Part == record )
+                    // parent is not missing
+                    && (record.parent == null || pawn.health.hediffSet.GetNotMissingParts().Contains( record.parent ))
+                    // parent is not an added part or is not solid (mainly for mutant arms and legs)
+                    && (record.parent == null || !pawn.health.hediffSet.HasDirectlyAddedPartFor( record.parent ) || !record.parent.def.IsSolid( record.parent, pawn.health.hediffSet.hediffs ))
+                    // is compatible with parent
+                    && !recipe.HasRestrictionsForPart( record, pawn.health.hediffSet )
+                    // part shouldn't be ignored
+                    && !pawn.health.hediffSet.PartShouldBeIgnored( record ) );
         }
 
         public override void ApplyOnPawn ( Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill )
@@ -36,7 +47,6 @@ namespace MSE2
             }
 
             // END VANILLA CODE
-
             else if ( pawn.Map != null )
             {
                 MedicalRecipesUtility.RestorePartAndSpawnAllPreviousParts( pawn, part, pawn.Position, pawn.Map );
