044d310a580a50baae952e2af2620de95a3037e9
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-04-22 17:40:28 +0200

    romoved prepare carefully related code

--- 1.1/Source/MedicalSystemExpansion/ModSpecificHarmony/PrepareCarefullyPatches/PrepareCarefullyPatches.cs
+++ 1.1/Source/MedicalSystemExpansion/ModSpecificHarmony/PrepareCarefullyPatches/PrepareCarefullyPatches.cs
@@ -1,45 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Reflection;
 
-using HarmonyLib;
+//using HarmonyLib;
 
-using EdB.PrepareCarefully;
+//using EdB.PrepareCarefully;
 
-namespace MSE2
-{
-    public static class PrepareCarefullyPatches
-    {
-        public static void ApplyPC ( Harmony harmony )
-        {
-            harmony.Patch( AddToPawn.MethodInfo(), AddToPawn.PreFix() );
-        }
+//namespace MSE2
+//{
+//    public static class PrepareCarefullyPatches
+//    {
+//        public static void ApplyPC ( Harmony harmony )
+//        {
+//            harmony.Patch( AddToPawn.MethodInfo(), AddToPawn.PreFix() );
+//        }
 
-        // Implant.AddToPawn
-        private static class AddToPawn
-        {
-            public static MethodInfo MethodInfo ()
-            {
-                return typeof( Implant ).GetMethod( nameof( Implant.AddToPawn ) );
-            }
+//        // Implant.AddToPawn
+//        private static class AddToPawn
+//        {
+//            public static MethodInfo MethodInfo ()
+//            {
+//                return typeof( Implant ).GetMethod( nameof( Implant.AddToPawn ) );
+//            }
 
-            public static HarmonyMethod PreFix ()
-            {
-                return new HarmonyMethod( typeof( AddToPawn ).GetMethod( nameof( ReplaceFunction ), BindingFlags.NonPublic | BindingFlags.Static ), Priority.Last );
-            }
+//            public static HarmonyMethod PreFix ()
+//            {
+//                return new HarmonyMethod( typeof( AddToPawn ).GetMethod( nameof( ReplaceFunction ), BindingFlags.NonPublic | BindingFlags.Static ), Priority.Last );
+//            }
 
-            private static bool ReplaceFunction ( Implant __instance, CustomPawn customPawn, Verse.Pawn pawn )
-            {
-                if ( __instance.recipe != null && __instance.BodyPartRecord != null )
-                {
-                    __instance.recipe.Worker.ApplyOnPawn( pawn, __instance.BodyPartRecord, null, null, null );
-                    pawn.health.capacities.Clear();
-                }
-                return false;
-            }
-        }
-    }
-}
+//            private static bool ReplaceFunction ( Implant __instance, CustomPawn customPawn, Verse.Pawn pawn )
+//            {
+//                if ( __instance.recipe != null && __instance.BodyPartRecord != null )
+//                {
+//                    __instance.recipe.Worker.ApplyOnPawn( pawn, __instance.BodyPartRecord, null, null, null );
+//                    pawn.health.capacities.Clear();
+//                }
+//                return false;
+//            }
+//        }
+//    }
+//}
